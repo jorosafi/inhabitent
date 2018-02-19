330e439ab9b8788bf43f86b0c422ef919f634944
Rodrigo Samayoa <jorosafi@gmail.com>
2018-02-19 15:37:21 -0800

    my first commit

---
+++ themes/inhabitent-theme/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="contact-footer">Test
+					<i class="far fa-address-card"></i>
+					<i class="fab fa-accessible-icon"></i>
+
+
+				</div>
+				<div class="site-info">
+					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
