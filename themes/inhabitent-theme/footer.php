f9a3ad2ea80707a33fa335a3652f6501eec378dd
Rodrigo Samayoa <jorosafi@gmail.com>
2018-02-19 21:11:03 -0800

    Add footer html and style article head

--- themes/inhabitent-theme/footer.php
+++ themes/inhabitent-theme/footer.php
@@ -10,12 +10,29 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="contact-footer">Test
-					<i class="far fa-address-card"></i>
-					<i class="fab fa-accessible-icon"></i>
 
+				<div class="footer-html">
+					<div class="business-info">
+						<h3>Contact Info</h3>
+						<p><i class="fa fa-envelope"></i> info@inhabitent.com</p>
+						<p><i class="fa fa-phone"></i> 778-456-7891</p>
+						<ul class="social-font-icons">
+							<li class="font-icons"><i class="fab fa-facebook-square"></i></li>
+							<li class="font-icons"><i class="fab fa-twitter-square"></i></li>
+							<li class="font-icons"><i class="fab fa-google-plus-square"></i></li>
+						</ul>
+					</div> 
 
-				</div>
+					<div class="business-hours">
+						<h3>Business Hours</h3>
+						<p><strong>Monday-Friday:</strong>9am to 5pm</p>
+						<p><strong>Saturday:</strong>10am to 2pm</p>
+						<p><strong>Sunday:</strong>Closed</p>
+					</div>
+
+					<div class="footer-logo"><img src="<?php echo get_template_directory_uri(); ?>/images/logo/inhabitent-logo-text.svg)"></div>
+				</div> <!--end footer-html -->
+
 				<div class="site-info">
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
