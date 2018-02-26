7cd91c71b0e3407da4e681649a30a11fbc039865
Rodrigo Samayoa <jorosafi@gmail.com>
2018-02-26 15:32:21 -0800

    creating custom post type for Adventures and adding to front page

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -75,6 +75,42 @@ get_header(); ?>
 				<?php endif?>
 			</div> <!-- Inhabitent Journal -->
 
+			<div class="adventure-posts">
+				<?php
+				// The Query
+				$args = array(
+					'post_type' => 'adventure_post_type',
+					'posts_per_page' => 4
+				);
+				$adventures = get_posts( $args );
+
+				// The Loop
+				if ( ! empty ($adventures) ) :?>
+
+					<h2>Latest Adventures</h2>
+					<ul>
+						<?php foreach ($adventures as $post) : setup_postdata ($post); ?>
+							<li>
+								<?php if (has_post_thumbnail()) : ?>
+									<div class="thumbnail-wrapper">
+										<?php the_post_thumbnail ('large'); ?>
+									</div>
+								<?php endif; ?>
+								<div class="entry-meta">
+									<div><?php red_starter_posted_on(); ?> / <span><?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?></span></div>
+								
+									<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+									<button class="black-btn-wrapper"><a class="black-btn" href="<?php the_permalink(); ?>">Read Entry</a></button>
+								</div>
+
+							</li>
+						<?php endforeach;
+						wp_reset_postdata(); ?>
+					</ul>
+				<?php endif?>
+			</div> <!-- Adventure Posts -->
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
---
+++ themes/inhabitent-theme/single-adventure_post_type.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+
+			<div class="social-share">
+				<button class="black-btn-wrapper"><a class="black-btn" href="<?php the_permalink(); ?>"><i class="fab fa-facebook-f"></i>Like</a></button>
+				<button class="black-btn-wrapper"><a class="black-btn" href="<?php the_permalink(); ?>"><i class="fab fa-twitter"></i>Tweet</a></button>
+				<button class="black-btn-wrapper"><a class="black-btn" href="<?php the_permalink(); ?>"><i class="fab fa-pinterest"></i>Pin</a></button>
+			</div>
+
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+				
+			?>
+		<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
